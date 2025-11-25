611bd2ee8df8487dd5d3c3553f7c0b57e93b1cf6
Salty Duck <142966149+Megaloduck@users.noreply.github.com>
2025-11-25 17:37:36 +0700

    Few upgraded

---
+++ Pages/LogisticsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Headquartz.Pages;
+
+public partial class LogisticsPage : ContentPage
+{
+	public LogisticsPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Pages/SettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Headquartz.Pages;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+}
