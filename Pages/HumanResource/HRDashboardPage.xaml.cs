b5637ed708513a23c5596f9ebd6c820cdce31983
Salty Duck <142966149+Megaloduck@users.noreply.github.com>
2025-12-07 18:11:38 +0700

    Better UI approach

--- Pages/HumanResource/HRDashboardPage.xaml.cs
+++ Pages/HumanResource/HRDashboardPage.xaml.cs
@@ -1,9 +1,12 @@
-namespace Headquartz.Pages.HumanResource;
+using Microsoft.Maui.Controls;
+using Headquartz.PageModels.HumanResource;
 
+namespace Headquartz.Pages.HumanResource;
 public partial class HRDashboardPage : ContentPage
 {
     public HRDashboardPage()
     {
         InitializeComponent();
-    }
+        //BindingContext ??= App.Current.Services.GetService<HRDashboardPageModel>();
+    }    
 }
